a62f9ee1ee35b0bffe177f12d7e8635282f2dcf3
Tommy Chang <zhangchuantao@bluemoon.com.cn>
2017-10-11 21:03:44 +0800

    Initial Commit

---
+++ TCAlgorithmsSummary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TCAlgorithmsSummary
+//
+//  Created by TC on 2017/10/11.
+//  Copyright © 2017年 Tommy Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TCAlgorithmsSummary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TCAlgorithmsSummary
+//
+//  Created by TC on 2017/10/11.
+//  Copyright © 2017年 Tommy Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
